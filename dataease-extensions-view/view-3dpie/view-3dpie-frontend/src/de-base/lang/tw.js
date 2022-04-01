8e20e3b9802520903aee34004e1fad8901b60662
fit2cloud-chenyw <yawen.chen@fit2cloud.com>
2022-04-01 14:19:46 +0800

    fix: 视图插件读取自定义国际化内容

--- dataease-extensions-view/view-3dpie/view-3dpie-frontend/src/de-base/lang/tw.js
+++ dataease-extensions-view/view-3dpie/view-3dpie-frontend/src/de-base/lang/tw.js
@@ -1,29 +1,7 @@
 export default {
-  plugin_ui: {
-    logo: '頭部系統 Logo',
-    loginLogo: '登陸頁面頭部 Logo',
-    loginImage: '登陸頁面右側圖片',
-    loginTitle: '登陸頁面標題',
-    title: '系統名稱',
-    upload: '上传',
-    clear: '清除',
-    save: '保存',
-    edit: '编辑',
-    cancel: '取消'
-  },
-  dept: {
-    can_not_move_change_sort: '不能移動以改變排序',
-    can_not_move_parent_to_children: '父組織不能移動到自己的子節點下',
-    move_success: '移動成功'
-  },
-  ukey: {
-    title: 'API Keys',
-    create: '創建',
-    create_success: '創建成功',
-    confirm_delete: '確定刪除？',
-    copied: '複製',
-    show: '顯示',
-    tips: '警告',
-    show_api: '查看API'
+  plugin_view_3d_pie: {
+    type_title: '3D餅圖',
+    label: '標籤',
+    angle: '角度'
   }
 }
--- dataease-extensions-view/view-bubblemap/view-bubblemap-frontend/src/de-base/lang/zh.js
+++ dataease-extensions-view/view-bubblemap/view-bubblemap-frontend/src/de-base/lang/zh.js
@@ -1,29 +1,8 @@
 export default {
-  plugin_ui: {
-    logo: '头部系统logo',
-    loginLogo: '登录页面头部logo',
-    loginImage: '登录页面右侧图片',
-    loginTitle: '登录页面标题',
-    title: '系统名称',
-    upload: '上传',
-    clear: '清除',
-    save: '保存',
-    edit: '编辑',
-    cancel: '取消'
-  },
-  dept: {
-    can_not_move_change_sort: '不能移动以改变排序',
-    can_not_move_parent_to_children: '父组织不能移动到自己的子节点下',
-    move_success: '移动成功'
-  },
-  ukey: {
-    title: 'API Keys',
-    create: '创建',
-    create_success: '创建成功',
-    confirm_delete: '确定删除？',
-    copied: '复制',
-    show: '显示',
-    tips: '警告',
-    show_api: '查看API'
+  plugin_view_buddle_map: {
+    type_title: '气泡地图',
+    area: '地区',
+    buddle_size: '气泡大小',
+    area_range: '地图范围'
   }
 }
