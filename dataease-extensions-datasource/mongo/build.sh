c2f66c5e41658e8d343f7815ef34c559c3d8ad73
taojinlong <jinlong@fit2cloud.com>
2022-10-31 18:20:29 +0800

    fix: 打包脚本

--- dataease-extensions-datasource/mongo/build.sh
+++ dataease-extensions-datasource/mongo/build.sh
@@ -3,4 +3,4 @@ mvn clean package -U -Dmaven.test.skip=true
 
 cp mongo-backend/target/mongo-backend-1.0-SNAPSHOT.jar .
 
-zip -r mongo.zip  ./mongo-backend-1.0-SNAPSHOT.jar ./mongoForBIDriver   ./plugin.json
+zip -r mongo.zip  ./mongo-backend-1.0-SNAPSHOT.jar ./mongobiDriver   ./plugin.json
