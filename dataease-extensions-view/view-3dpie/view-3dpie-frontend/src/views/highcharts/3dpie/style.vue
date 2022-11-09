6e627d848e074249a88b38afe2596000b6759f94
fit2cloud-chenyw <yawen.chen@fit2cloud.com>
2022-11-09 14:35:18 +0800

    perf: 视图插件样式

--- dataease-extensions-view/view-3dpie/view-3dpie-frontend/src/views/highcharts/3dpie/style.vue
+++ dataease-extensions-view/view-3dpie/view-3dpie-frontend/src/views/highcharts/3dpie/style.vue
@@ -1,30 +1,24 @@
 <template>
   <div
-    style="overflow:auto;border-right: 1px solid #e6e6e6;height: 100%;width: 100%;"
+    style="
+      overflow: auto;
+      border-right: 1px solid #e6e6e6;
+      height: 100%;
+      width: 100%;
+    "
     class="attr-style theme-border-class"
   >
-    <el-row class="padding-lr">
-      <span class="title-text">{{ $t('chart.style_priority') }}</span>
-      <el-row>
-        <el-radio-group
-          v-model="view.stylePriority"
-          class="radio-span"
-          size="mini"
-          @change="calcStyle"
-        >
-          <el-radio label="view"><span>{{ $t('chart.chart') }}</span></el-radio>
-          <el-radio label="panel"><span>{{ $t('chart.dashboard') }}</span></el-radio>
-        </el-radio-group>
-      </el-row>
-    </el-row>
-
     <el-row>
-      <span class="padding-lr">{{ $t('chart.shape_attr') }}</span>
+      <span class="padding-lr">{{ $t("chart.shape_attr") }}</span>
       <el-collapse v-model="attrActiveNames" class="style-collapse">
         <el-collapse-item name="color" :title="$t('chart.color')">
-          <color-selector :param="param" class="attr-selector" :chart="chart" @onColorChange="onColorChange" />
+          <color-selector
+            :param="param"
+            class="attr-selector"
+            :chart="chart"
+            @onColorChange="onColorChange"
+          />
         </el-collapse-item>
-
 
         <el-collapse-item name="label" :title="$t('chart.label')">
           <label-selector
@@ -35,8 +29,7 @@
           />
         </el-collapse-item>
 
-
-        <el-collapse-item name="tooltip" :title="$t('chart.tooltip')" >
+        <el-collapse-item name="tooltip" :title="$t('chart.tooltip')">
           <tooltip-selector
             :param="param"
             class="attr-selector"
@@ -48,12 +41,14 @@
     </el-row>
 
     <el-row>
-      <span class="padding-lr">{{ $t('chart.module_style') }}</span>
+      <span class="padding-lr">{{ $t("chart.module_style") }}</span>
       <el-collapse v-model="styleActiveNames" class="style-collapse">
-
-        <el-collapse-item v-show="view.type" name="title" :title="$t('chart.title')">
+        <el-collapse-item
+          v-show="view.type"
+          name="title"
+          :title="$t('chart.title')"
+        >
           <title-selector
-
             :param="param"
             class="attr-selector"
             :chart="chart"
@@ -61,17 +56,13 @@
           />
         </el-collapse-item>
 
-        <el-collapse-item
-          name="legend"
-          :title="$t('chart.legend')"
-        >
-            <legend-selector
-                
-                :param="param"
-                class="attr-selector"
-                :chart="chart"
-                @onLegendChange="onLegendChange"
-            />
+        <el-collapse-item name="legend" :title="$t('chart.legend')">
+          <legend-selector
+            :param="param"
+            class="attr-selector"
+            :chart="chart"
+            @onLegendChange="onLegendChange"
+          />
         </el-collapse-item>
 
         <el-collapse-item name="background" :title="$t('chart.background')">
@@ -88,94 +79,93 @@
 </template>
 
 <script>
-  import ColorSelector from '@/components/selector/ColorSelector'
-  import LabelSelector from '@/components/selector/LabelSelector'
-  import LegendSelector from '@/components/selector/LegendSelector'
-  import TitleSelector from '@/components/selector/TitleSelector'
-  import TooltipSelector from '@/components/selector/TooltipSelector'
-  import BackgroundColorSelector from '@/components/selector/BackgroundColorSelector'
-  export default {
-    components: {
-      ColorSelector,
-      LabelSelector,
-      LegendSelector,
-      TitleSelector,
-      TooltipSelector,
-      BackgroundColorSelector
+import ColorSelector from "@/components/selector/ColorSelector";
+import LabelSelector from "@/components/selector/LabelSelector";
+import LegendSelector from "@/components/selector/LegendSelector";
+import TitleSelector from "@/components/selector/TitleSelector";
+import TooltipSelector from "@/components/selector/TooltipSelector";
+import BackgroundColorSelector from "@/components/selector/BackgroundColorSelector";
+export default {
+  components: {
+    ColorSelector,
+    LabelSelector,
+    LegendSelector,
+    TitleSelector,
+    TooltipSelector,
+    BackgroundColorSelector,
+  },
+  data() {
+    return {
+      attrActiveNames: [],
+      styleActiveNames: [],
+    };
+  },
+  props: {
+    obj: {
+      type: Object,
+      default: () => {},
     },
-    data() {
-      return {
-        attrActiveNames: [],
-        styleActiveNames: [],
-      }
-    },
-    props: {
+  },
 
-      obj: {
-        type: Object,
-        default: () => {}
-      }
+  computed: {
+    param() {
+      return this.obj.param;
+    },
+    view() {
+      return this.obj.view;
+    },
+    chart() {
+      return this.obj.chart;
+    },
+  },
+  methods: {
+    onColorChange(val) {
+      this.view.customAttr.color = val;
+      this.calcStyle();
+    },
+    onLabelChange(val) {
+      this.view.customAttr.label = val;
+      this.calcStyle();
     },
 
-    computed: {
-      param() {
-        return this.obj.param
-      },
-      view() {
-        return this.obj.view
-      },
-      chart() {
-        return this.obj.chart
-      }
+    onTooltipChange(val) {
+      this.view.customAttr.tooltip = val;
+      this.calcStyle();
     },
-    methods: {
-      onColorChange(val) {
-        this.view.customAttr.color = val
-        this.calcStyle()
-      },
-      onLabelChange(val) {
-        this.view.customAttr.label = val
-        this.calcStyle()
-      },
-
-      onTooltipChange(val) {
-        this.view.customAttr.tooltip = val
-        this.calcStyle()
-      },
-      onTextChange(val) {
-        this.view.customStyle.text = val
-        this.view.title = val.title
-        this.calcStyle()
-      },
-      onChangeBackgroundForm(val) {
-        this.view.customStyle.background = val
-        this.calcStyle()
-      },
-      onLegendChange(val) {
-        this.view.customStyle.legend = val
-        this.calcStyle()
-      },
-      calcStyle() {
-        this.$emit('plugin-call-back', {
-          eventName: 'plugins-calc-style',
-          eventParam: this.view
-        })
-      },
-    }
-  }
+    onTextChange(val) {
+      this.view.customStyle.text = val;
+      this.view.title = val.title;
+      this.calcStyle();
+    },
+    onChangeBackgroundForm(val) {
+      this.view.customStyle.background = val;
+      this.calcStyle();
+    },
+    onLegendChange(val) {
+      this.view.customStyle.legend = val;
+      this.calcStyle();
+    },
+    calcStyle() {
+      this.$emit("plugin-call-back", {
+        eventName: "plugins-calc-style",
+        eventParam: this.view,
+      });
+    },
+  },
+};
 </script>
 
 <style lang="scss" scoped>
-  .padding-lr {
-    padding: 0 6px;
-  }
-  span {
-    font-size: 12px;
-  }
-  .el-radio {
-    margin: 5px;
-  }
-  .radio-span > > > .el-radio__label {
-    margin-left: 2px;
-  }
+.padding-lr {
+  padding: 0 6px;
+}
+span {
+  font-size: 12px;
+}
+.el-radio {
+  margin: 5px;
+}
+.radio-span ::v-deep .el-radio__label {
+  margin-left: 2px;
+}
 </style>
