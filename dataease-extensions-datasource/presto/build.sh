b8b6d625c16399741d058000baac7c84882ce578
fit2cloud-chenyw <yawen.chen@fit2cloud.com>
2022-12-19 20:18:04 +0800

    fix: 发布版本号规范化

--- dataease-extensions-datasource/presto/build.sh
+++ dataease-extensions-datasource/presto/build.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 mvn clean package
 
-cp presto-backend/target/presto-backend-1.0-SNAPSHOT.jar .
+cp presto-backend/target/presto-backend-1.18.0.jar .
 
-zip -r presto.zip  ./presto-backend-1.0-SNAPSHOT.jar ./prestoDriver   ./plugin.json
+zip -r presto.zip  ./presto-backend-1.18.0.jar ./prestoDriver   ./plugin.json
