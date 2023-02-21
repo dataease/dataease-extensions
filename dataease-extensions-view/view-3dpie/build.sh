dc36abb088a04ae15e686b08f18548c4c7a7fb55
fit2cloud-chenyw <yawen.chen@fit2cloud.com>
2023-02-21 13:00:13 +0800

    style(符号地图): 样式编辑页面字号不统一

--- dataease-extensions-view/view-3dpie/build.sh
+++ dataease-extensions-view/view-3dpie/build.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 mvn clean package
 
-cp view-3dpie-backend/target/view-3dpie-backend-1.18.0.jar .
+cp view-3dpie-backend/target/view-3dpie-backend-1.18.3.jar .
 
-zip -r 3dpie.zip  ./view-3dpie-backend-1.18.0.jar ./plugin.json
+zip -r 3dpie.zip  ./view-3dpie-backend-1.18.3.jar ./plugin.json
 
-rm -f ./view-3dpie-backend-1.18.0.jar
+rm -f ./view-3dpie-backend-1.18.3.jar
