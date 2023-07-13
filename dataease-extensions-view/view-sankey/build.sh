30ebc20f84bbd042a9e28ab6a799dea9baef14c0
ulleo <ulleo.wang@gmail.com>
2023-07-13 16:16:01 +0800

    build: 桑基图build.sh

--- dataease-extensions-view/view-sankey/build.sh
+++ dataease-extensions-view/view-sankey/build.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 mvn clean package
 
-cp view-sankey-backend/target/view-sankey-backend-1.18.3.jar .
+cp view-sankey-backend/target/view-sankey-backend-1.18.9.jar .
 
-zip -r sankey.zip  ./view-sankey-backend-1.18.3.jar ./plugin.json
+zip -r sankey.zip  ./view-sankey-backend-1.18.9.jar ./plugin.json
 
-rm -f ./view-sankey-backend-1.18.3.jar
+rm -f ./view-sankey-backend-1.18.9.jar
